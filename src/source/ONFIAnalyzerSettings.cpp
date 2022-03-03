da0a8a91c4f010e7481dba760749ef232d8825e0
mona-quimamba <claudio.antonio@etu.unilim.fr>
2022-03-03 18:14:02 +0100

    début du code de la classe settings pas encore fini

--- src/source/ONFIAnalyzerSettings.cpp
+++ src/source/ONFIAnalyzerSettings.cpp
@@ -1,14 +1,77 @@
 #include "ONFIAnalyzerSettings.h"
 #include "../AnalyzerSDK/include/AnalyzerHelpers.h"
 
+/**
+* @brief consctructor it'll build the settings for our analyzer , set up  each signal on one channel
+*
+*********************************/
+ONFIAnalyzerSettings::ONFIAnalyzerSettings():
 
-ONFIAnalyzerSettings::ONFIAnalyzerSettings()
-:	mInputChannel( UNDEFINED_CHANNEL ),
-	mBitRate( 9600 )
+	 mALEChannel( UNDEFINED_CHANNEL ),
+	 mCEChannel( UNDEFINED_CHANNEL ),
+	 mCLEChannel( UNDEFINED_CHANNEL ),
+	 mLOCKChannel( UNDEFINED_CHANNEL ),
+	 mREChannel( UNDEFINED_CHANNEL ),
+	 mWEChannel( UNDEFINED_CHANNEL ),
+	 mWPChannel( UNDEFINED_CHANNEL ),
+	 mIOChannel( UNDEFINED_CHANNEL ),
+	 mRBChannel( UNDEFINED_CHANNEL ),
+	 mBitRate( 9600 )
 {
-	mInputChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
-	mInputChannelInterface->SetTitleAndTooltip( "Serial", "Standard Protocole-ONFI" );
-	mInputChannelInterface->SetChannel( mInputChannel );
+
+
+	mALEChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
+	mALEChannelInterface->SetTitleAndTooltip( "ALE", "INPUT , Address latch enable" );
+	mALEChannelInterface->SetChannel( mALEChannel );
+	mALEChannelInterface->SetSelectionOfNoneIsAllowed( true );
+
+	mCEChannel.reset( new AnalyzerSettingInterfaceChannel() );
+	mCEChannelInterface->SetTitleAndTooltip( "CE", "INPUT , Chip enable" );
+	mCEChannelInterface->SetChannel( mCEChannel );
+	mCEChannelInterface->SetSelectionOfNoneIsAllowed( true );
+
+	mCLEChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
+	mCLEChannelInterface->SetTitleAndTooltip( "CLE", "INPUT, Command latch enable" );
+	mCLEChannelInterface->SetChannel( mCLEChannel );
+	mCLEChannelInterface->SetSelectionOfNoneIsAllowed( true );
+
+
+	mLOCKChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
+	mLOCKChannelInterface->SetTitleAndTooltip( "LOCK", "INPUT, " );
+	mLOCKChannelInterface->SetChannel( mLOCKChannel );
+	mLOCKChannelInterface->SetSelectionOfNoneIsAllowed( true );
+
+
+	mREChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
+	mREChannelInterface->SetTitleAndTooltip( "RE", "INPUT, READ enable" );
+	mREChannelInterface->SetChannel( mREChannel );
+	mREChannelInterface->SetSelectionOfNoneIsAllowed( true );
+
+	mWEChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
+	mWEChannelInterface->SetTitleAndTooltip( "WE", "INPUT, Write enable " );
+	mWEChannelInterface->SetChannel( mWEChannel );
+	mWEChannelInterface->SetSelectionOfNoneIsAllowed( true );
+
+
+	mWPChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
+	mWPChannelInterface->SetTitleAndTooltip( "WP", "INPUT, Write protect" );
+	mWPChannelInterface->SetChannel( mWPChannel );
+	mWPChannelInterface->SetSelectionOfNoneIsAllowed( true );
+
+	mIOChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
+	mIOChannelInterface->SetTitleAndTooltip( "IO", "I/O, Data inputs/outputs" );
+	mIOChannelInterface->SetChannel( mIOChannel );
+	mIOChannelInterface->SetSelectionOfNoneIsAllowed( true );
+
+	mRBChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
+	mRBChannelInterface->SetTitleAndTooltip( "RB", "OUTPUT, Ready/busy" );
+	mRBChannelInterface->SetChannel( mRBChannel );
+	mRBChannelInterface->SetSelectionOfNoneIsAllowed( true );
+
+
+	//****************
+
+
 
 	mBitRateInterface.reset( new AnalyzerSettingInterfaceInteger() );
 	mBitRateInterface->SetTitleAndTooltip( "Bit Rate (Bits/S)",  "Specify the bit rate in bits per second." );
@@ -16,21 +79,49 @@ ONFIAnalyzerSettings::ONFIAnalyzerSettings()
 	mBitRateInterface->SetMin( 1 );
 	mBitRateInterface->SetInteger( mBitRate );
 
-	AddInterface( mInputChannelInterface.get() );
+	//****
+
+	AddInterface( mALEChannelInterface.get() );
+	AddInterface( mCEChannelInterface.get() );
+	AddInterface( mCLEChannelInterface.get() );
+	AddInterface( mLOCKChannelInterface.get() );
+	AddInterface( mREChannelInterface.get() );
+	AddInterface( mWEChannelInterface.get() );
+	AddInterface( mWPChannelInterface.get() );
+	AddInterface( mIOChannelInterface.get() );
+	AddInterface( mRBChannelInterface.get() );
+
+
 	AddInterface( mBitRateInterface.get() );
 
+//****************** Export datas
 	AddExportOption( 0, "Export as text/csv file" );
 	AddExportExtension( 0, "text", "txt" );
 	AddExportExtension( 0, "csv", "csv" );
 
 	ClearChannels();
-	AddChannel( mInputChannel, "Serial", false );
+	AddChannel( mALEChannel, "ALE", false );
+	AddChannel( mCEChannel, "CE", false );
+	AddChannel( mCLEChannel, "CLE", false );
+	AddChannel( mLOCKChannel, "LOCK", false );
+	AddChannel( mREChannel, "RE", false );
+	AddChannel( mWEChannel, "WE", false );
+	AddChannel( mWPChannel, "WP", false );
+	AddChannel( mIOChannel, "I/O", false );
+	AddChannel( mRBChannel, "RB", false );
 }
 
+/**
+*  @brief Destructor we won't to do anything
+*************************/
 ONFIAnalyzerSettings::~ONFIAnalyzerSettings()
 {
 }
 
+/**
+*  @brief this function will copy the values saved in our interface ojbects to
+*  our settings variables
+**********************/
 bool ONFIAnalyzerSettings::SetSettingsFromInterfaces()
 {
 	mInputChannel = mInputChannelInterface->GetChannel();
--- src/source/ONFIAnalyzerSettings.h
+++ src/source/ONFIAnalyzerSettings.h
@@ -1,6 +1,7 @@
 #ifndef ONFI_ANALYZER_SETTINGS
 #define ONFI_ANALYZER_SETTINGS
 
+
 #include "../AnalyzerSDK/include/AnalyzerSettings.h"
 #include "../AnalyzerSDK/include/AnalyzerTypes.h"
 
@@ -20,18 +21,35 @@ class ONFIAnalyzerSettings : public AnalyzerSettings
 public:
 	ONFIAnalyzerSettings();
 	virtual ~ONFIAnalyzerSettings();
-
 	virtual bool SetSettingsFromInterfaces();
 	void UpdateInterfacesFromSettings();
 	virtual void LodSettings( const char* settings );
 	virtual const char* SaveSettings();
 
 
-	Channel mInputChannel;
+	Channel mALEChannel;
+	Channel mCEChannel;
+	Channel mCLEChannel;
+	Channel mLOCKChannel;
+	Channel mREChannel;
+	Channel mWEChannel;
+	Channel mWPChannel;
+	Channel mIOChannel;
+	Channel mRBChannel;
+
 	U32 mBitRate;
 
 protected:
-	std::auto_ptr< AnalyzerSettingInterfaceChannel >	mInputChannelInterface;
+
+	std::auto_ptr< AnalyzerSettingInterfaceChannel > mALEChannelInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceChannel > mCEChannelInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceChannel > mCLEChannelInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceChannel > mLOCKChannelInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceChannel > mREChannelInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceChannel >  mWEChannelInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceChannel > mWPChannelInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceChannel > mIOChannelInterface;
+	std::auto_ptr< AnalyzerSettingInterfaceChannel > mRBChannelInterface;
 	std::auto_ptr< AnalyzerSettingInterfaceInteger >	mBitRateInterface;
 };
 
