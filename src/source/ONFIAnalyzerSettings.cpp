ef1b7e4a27bd2f8f609b9e0b6aeac0e6da09ced7
mona-quimamba <claudio.antonio@etu.unilim.fr>
2022-03-05 00:15:17 +0100

    fin de la classe Settings à changer si besoin selon les contraintes du protocol onfi

--- src/source/ONFIAnalyzerSettings.cpp
+++ src/source/ONFIAnalyzerSettings.cpp
@@ -35,76 +35,83 @@ ONFIAnalyzerSettings::ONFIAnalyzerSettings()
 	mIOChannel( UNDEFINED_CHANNEL ),
 	mRBChannel( UNDEFINED_CHANNEL )
 {
-
+    /// @details Setting up each AnalyzerSettingInterface object
+		// for the moment only AnalyzerSettingInterfaceChannel
 		mALEChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
 		mALEChannelInterface->SetTitleAndTooltip( "ALE", "INPUT , Address latch enable" );
 		mALEChannelInterface->SetChannel( mALEChannel );
 		mALEChannelInterface->SetSelectionOfNoneIsAllowed( true );
 
-	mCEChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
-	mCEChannelInterface->SetTitleAndTooltip( "CE", "INPUT , Chip enable" );
-	mCEChannelInterface->SetChannel( mCEChannel );
-	mCEChannelInterface->SetSelectionOfNoneIsAllowed( true );
+		mCEChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
+		mCEChannelInterface->SetTitleAndTooltip( "CE", "INPUT , Chip enable" );
+		mCEChannelInterface->SetChannel( mCEChannel );
+		mCEChannelInterface->SetSelectionOfNoneIsAllowed( true );
 
-	mCLEChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
-	mCLEChannelInterface->SetTitleAndTooltip( "CLE", "INPUT, Command latch enable" );
-	mCLEChannelInterface->SetChannel( mCLEChannel );
-	mCLEChannelInterface->SetSelectionOfNoneIsAllowed( true );
-
-
-	mLOCKChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
-	mLOCKChannelInterface->SetTitleAndTooltip( "LOCK", "INPUT, " );
-	mLOCKChannelInterface->SetChannel( mLOCKChannel );
-	mLOCKChannelInterface->SetSelectionOfNoneIsAllowed( true );
+		mCLEChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
+		mCLEChannelInterface->SetTitleAndTooltip( "CLE", "INPUT, Command latch enable" );
+		mCLEChannelInterface->SetChannel( mCLEChannel );
+		mCLEChannelInterface->SetSelectionOfNoneIsAllowed( true );
 
 
-	mREChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
-	mREChannelInterface->SetTitleAndTooltip( "RE", "INPUT, READ enable" );
-	mREChannelInterface->SetChannel( mREChannel );
-	mREChannelInterface->SetSelectionOfNoneIsAllowed( true );
-
-	mWEChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
-	mWEChannelInterface->SetTitleAndTooltip( "WE", "INPUT, Write enable " );
-	mWEChannelInterface->SetChannel( mWEChannel );
-	mWEChannelInterface->SetSelectionOfNoneIsAllowed( true );
+		mLOCKChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
+		mLOCKChannelInterface->SetTitleAndTooltip( "LOCK", "INPUT, " );
+		mLOCKChannelInterface->SetChannel( mLOCKChannel );
+		mLOCKChannelInterface->SetSelectionOfNoneIsAllowed( true );
 
 
-	mWPChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
-	mWPChannelInterface->SetTitleAndTooltip( "WP", "INPUT, Write protect" );
-	mWPChannelInterface->SetChannel( mWPChannel );
-	mWPChannelInterface->SetSelectionOfNoneIsAllowed( true );
+		mREChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
+		mREChannelInterface->SetTitleAndTooltip( "RE", "INPUT, READ enable" );
+		mREChannelInterface->SetChannel( mREChannel );
+		mREChannelInterface->SetSelectionOfNoneIsAllowed( true );
 
-	mIOChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
-	mIOChannelInterface->SetTitleAndTooltip( "IO", "I/O, Data inputs/outputs" );
-	mIOChannelInterface->SetChannel( mIOChannel );
-	mIOChannelInterface->SetSelectionOfNoneIsAllowed( true );
-
-	mRBChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
-	mRBChannelInterface->SetTitleAndTooltip( "RB", "OUTPUT, Ready/busy" );
-	mRBChannelInterface->SetChannel( mRBChannel );
-	mRBChannelInterface->SetSelectionOfNoneIsAllowed( true );
+		mWEChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
+		mWEChannelInterface->SetTitleAndTooltip( "WE", "INPUT, Write enable " );
+		mWEChannelInterface->SetChannel( mWEChannel );
+		mWEChannelInterface->SetSelectionOfNoneIsAllowed( true );
 
 
+		mWPChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
+		mWPChannelInterface->SetTitleAndTooltip( "WP", "INPUT, Write protect" );
+		mWPChannelInterface->SetChannel( mWPChannel );
+		mWPChannelInterface->SetSelectionOfNoneIsAllowed( true );
+
+		mIOChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
+		mIOChannelInterface->SetTitleAndTooltip( "IO", "I/O, Data inputs/outputs" );
+		mIOChannelInterface->SetChannel( mIOChannel );
+		mIOChannelInterface->SetSelectionOfNoneIsAllowed( true );
+
+		mRBChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
+		mRBChannelInterface->SetTitleAndTooltip( "RB", "OUTPUT, Ready/busy" );
+		mRBChannelInterface->SetChannel( mRBChannel );
+		mRBChannelInterface->SetSelectionOfNoneIsAllowed( true );
 
 
-	AddInterface( mALEChannelInterface.get() );
-	AddInterface( mCEChannelInterface.get() );
-	AddInterface( mCLEChannelInterface.get() );
-	AddInterface( mLOCKChannelInterface.get() );
-	AddInterface( mREChannelInterface.get() );
-	AddInterface( mWEChannelInterface.get() );
-	AddInterface( mWPChannelInterface.get() );
-	AddInterface( mIOChannelInterface.get() );
-	AddInterface( mRBChannelInterface.get() );
 
 
-	//AddInterface( mBitRateInterface.get() );
+		AddInterface( mALEChannelInterface.get() );
+		AddInterface( mCEChannelInterface.get() );
+		AddInterface( mCLEChannelInterface.get() );
+		AddInterface( mLOCKChannelInterface.get() );
+		AddInterface( mREChannelInterface.get() );
+		AddInterface( mWEChannelInterface.get() );
+		AddInterface( mWPChannelInterface.get() );
+		AddInterface( mIOChannelInterface.get() );
+		AddInterface( mRBChannelInterface.get() );
 
-//****************** Export datas
+
+
+
+/// @details Specifying the export options
 	AddExportOption( 0, "Export as text/csv file" );
 	AddExportExtension( 0, "text", "txt" );
 	AddExportExtension( 0, "csv", "csv" );
 
+/// @details Specifying which channels are in use
+/*
+* The analyzer must indicate which channel(s) it is using.
+* This is done with the AddChannel function.
+ Every time the channel changes (such as when the user changes the channel) the reported channel must be updated. To clear any previous channels that have been set, call ClearChannels.
+*/
 	ClearChannels();
 	AddChannel( mALEChannel, "ALE", false );
 	AddChannel( mCEChannel, "CE", false );
@@ -130,6 +137,15 @@ ONFIAnalyzerSettings::~ONFIAnalyzerSettings()
 *  @brief this function will copy the values saved in our interface ojbects to
 *  our settings variables
 **********************/
+/*
+For example, when using more than one channel,
+you would typically want to make
+ sure that all the channels are different.
+You can use the AnalyzerHelpers::DoChannelsOverlap
+function to make that easier if you like
+
+à faire
+*/
 bool ONFIAnalyzerSettings::SetSettingsFromInterfaces()
 {
 	mALEChannel = mALEChannelInterface->GetChannel();
@@ -146,30 +162,69 @@ bool ONFIAnalyzerSettings::SetSettingsFromInterfaces()
 
 void ONFIAnalyzerSettings::UpdateInterfacesFromSettings()
 {
-	mALEChannelInterface->SetChannel( mALEChannel );
-	//mBitRateInterface->SetInteger( mBitRate );*/
+		mALEChannelInterface->SetChannel( mALEChannel );
+		mCEChannelInterface->SetChannel( mCEChannel );
+		mCLEChannelInterface->SetChannel( mCLEChannel );
+		mLOCKChannelInterface->SetChannel( mLOCKChannel );
+		mREChannelInterface->SetChannel( mREChannel );
+		mWEChannelInterface->SetChannel( mWEChannel );
+		mWPChannelInterface->SetChannel( mWPChannel );
+		mIOChannelInterface->SetChannel( mIOChannel );
+		mRBChannelInterface->SetChannel( mRBChannel );
+
 }
+
+/**
+*  @brief this function will  serialize all variables set upped
+*
+******/
 
 void ONFIAnalyzerSettings::LoadSettings( const char* settings )
 {
+	// object used for serialize the variables
 	SimpleArchive text_archive;
 	text_archive.SetString( settings );
 
 	text_archive >> mALEChannel;
-	//text_archive >> mBitRate;
-
+	text_archive >> mCEChannel;
+	text_archive >> mCLEChannel;
+	text_archive >> mLOCKChannel;
+	text_archive >> mREChannel;
+	text_archive >> mWEChannel;
+	text_archive >> mWPChannel;
+	text_archive >> mIOChannel;
+	text_archive >> mRBChannel;
+/// @brief  Since our channel values may have changed, we will also need to update the channels we’re reporting as using. We need to do this every times settings change.
 	ClearChannels();
-	AddChannel( mALEChannel, "Protocole-ONFI", true );
-
+	AddChannel( mALEChannel, "Protocole-ONFI", mALEChannel != UNDEFINED_CHANNEL );
+	AddChannel( mCEChannel, "Protocole-ONFI", mCEChannel != UNDEFINED_CHANNEL );
+	AddChannel( mCLEChannel, "Protocole-ONFI", mCLEChannel != UNDEFINED_CHANNEL );
+	AddChannel( mLOCKChannel, "Protocole-ONFI", mLOCKChannel != UNDEFINED_CHANNEL );
+	AddChannel( mREChannel, "Protocole-ONFI", mREChannel != UNDEFINED_CHANNEL );
+	AddChannel( mWEChannel, "Protocole-ONFI", mWEChannel != UNDEFINED_CHANNEL );
+	AddChannel( mWPChannel, "Protocole-ONFI", mWPChannel != UNDEFINED_CHANNEL );
+	AddChannel( mIOChannel, "Protocole-ONFI", mIOChannel != UNDEFINED_CHANNEL );
+	AddChannel( mRBChannel, "Protocole-ONFI", mRBChannel != UNDEFINED_CHANNEL );
+ /// @bried This will update all our interfaces to reflect the newly loaded values.
 	UpdateInterfacesFromSettings();
 }
 
+/**
+* @breif this function will save all of our settings variables into a single string
+*****/
 const char* ONFIAnalyzerSettings::SaveSettings()
 {
 	SimpleArchive text_archive;
 
 	text_archive << mALEChannel;
-	//text_archive << mBitRate;
+	text_archive << mCEChannel;
+	text_archive << mCLEChannel;
+	text_archive << mLOCKChannel;
+	text_archive << mREChannel;
+	text_archive << mWEChannel;
+	text_archive << mWPChannel;
+	text_archive << mIOChannel;
+	text_archive << mRBChannel;
 
 	return SetReturnString( text_archive.GetString() );
 }
